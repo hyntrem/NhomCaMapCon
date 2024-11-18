ce2a7090017c245032a9bc51d16d7586a780601f
hyntrem <tramnh3098@ut.edu.vn>
2024-11-19 00:12:17 +0700

    init project

---
+++ CaKoi/CaKoi/Controllers/AccessController1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CaKoi.Controllers;
+
+namespace CaKoi.Controllers
+{
+    public class AccessController1 : Controller
+
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (HttpContent.Session.GetString("UserName") == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Indext", "Home");
+            }
+        }
+        [HttpGet]
+        public IActionResult Login(TUser user) 
+        {
+            if (HttpContext.Session.GetString("UserName") == Null)
+            {
+                var u = db.TUser.Where(X => X.Username.Equals(user.Username) && x.Password.Equals(user.Password)).FirsttOrDefault();
+                if(u! = null)
+                {
+                    HttpContext.Session.SetString("UserName", u.UserName.ToString());
+                    return RedirectToAction("Indext", "Home");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ CaKoi/CaKoi/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using CaKoi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using X.PagedList;
+
+namespace CaKoi.Controllers
+{
+    public class HomeController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(X=>X.TenSp);
+            PagedList<TDanhMucSp> Lst = new PagedList<TDanhMucSp>(lstsanpham, pageNumber, pageSize);
+            return View(lst);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
